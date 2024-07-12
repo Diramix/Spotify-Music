6de06b76c30032badcc69153f40fc0238339a4b0
Diramix <79011730+Diramix@users.noreply.github.com>
2024-07-12 20:48:03 +0300

    Public 1.6

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ setInterval(() => {
     if (imgBackground) {
         const targetElement = document.querySelector('.FullscreenPlayerDesktop_content__bl_7V');
         if (targetElement) {
-            targetElement.style.background = `linear-gradient(180deg, rgba(0, 0, 0, 0.2) 0%, rgba(0, 0, 0, 0.95) 100%), url(${imgBackground}) center center / cover no-repeat`;
+            targetElement.style.background = `linear-gradient(180deg, rgba(0, 0, 0, 0.35) 0%, rgba(0, 0, 0, 0.75) 100%), url(${imgBackground}) center center / cover no-repeat`;
             console.log(targetElement)
         }
     }
