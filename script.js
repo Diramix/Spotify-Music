40a93066d647996d49700f7a65f5ce2357afad42
Diramix <79011730+Diramix@users.noreply.github.com>
2024-07-17 20:05:19 +0300

    Public 1.8

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ setInterval(() => {
     imgElements.forEach(img => {
         if (img.src && img.src.includes('/400x400')) {
             imgBackground = img.src.replace('/400x400', '/1000x1000');
-            console.log(imgBackground)
         }
     });
 
@@ -13,7 +12,6 @@ setInterval(() => {
         const targetElement = document.querySelector('.FullscreenPlayerDesktop_content__bl_7V');
         if (targetElement) {
             targetElement.style.background = `linear-gradient(180deg, rgba(0, 0, 0, 0.35) 0%, rgba(0, 0, 0, 0.75) 100%), url(${imgBackground}) center center / cover no-repeat`;
-            console.log(targetElement)
         }
     }
-}, 1000);
+}, 0);
