802997ae7f7f764cd4aaa3216f19119e0c96062f
Diramix <79011730+Diramix@users.noreply.github.com>
2024-07-30 18:44:47 +0300

    Public 1.12

--- Spotify Music!/script.js
+++ Spotify Music!/script.js
@@ -1,17 +1,19 @@
 setInterval(() => {
-    const imgElements = document.querySelectorAll('[class*="FullscreenPoster_cover"]');
+    const imgElements = document.querySelectorAll('[class*="FullscreenPlayerDesktopPoster_cover"]');
     let imgBackground = "";
 
     imgElements.forEach(img => {
         if (img.src && img.src.includes('/400x400')) {
             imgBackground = img.src.replace('/400x400', '/1000x1000');
+            console.log(imgBackground)
         }
     });
 
     if (imgBackground) {
-        const targetElement = document.querySelector('.FullscreenPlayerDesktop_content__bl_7V');
+        const targetElement = document.querySelector('.FullscreenPlayerDesktop_modalContent__Zs_LC');
         if (targetElement) {
-            targetElement.style.background = `linear-gradient(180deg, rgba(0, 0, 0, 0.10) 0%, rgba(0, 0, 0, 0.75) 100%), url(${imgBackground}) center center / cover no-repeat`;
+            targetElement.style.background = `linear-gradient(180deg, rgba(0, 0, 0, 0.30) 0%, rgba(0, 0, 0, 0.75) 100%), url(${imgBackground}) center center / cover no-repeat`;
+            console.log(targetElement)
         }
     }
-}, 0);
+}, 1000);
