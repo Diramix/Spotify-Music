f10c0237972650b528f5125ca572bea43aa4bb9e
Diramix <danyagranny10@gmail.com>
2024-08-08 14:54:32 +0300

    optimize script

--- Spotify Music!/script.js
+++ Spotify Music!/script.js
@@ -5,15 +5,30 @@ setInterval(() => {
     imgElements.forEach(img => {
         if (img.src && img.src.includes('/400x400')) {
             imgBackground = img.src.replace('/400x400', '/1000x1000');
-            console.log(imgBackground)
         }
     });
 
     if (imgBackground) {
         const targetElement = document.querySelector('.FullscreenPlayerDesktop_modalContent__Zs_LC');
         if (targetElement) {
-            targetElement.style.background = `linear-gradient(180deg, rgba(0, 0, 0, 0.30) 0%, rgba(0, 0, 0, 0.75) 100%), url(${imgBackground}) center center / cover no-repeat`;
-            console.log(targetElement)
+
+            const currentBackground = targetElement.style.background;
+
+            const newBackground = `linear-gradient(180deg, rgba(0, 0, 0, 0.30) 0%, rgba(0, 0, 0, 0.75) 100%), url(${imgBackground}) center center / cover no-repeat`;
+
+            const img = new Image();
+            img.src = imgBackground;
+
+            img.onload = () => {
+
+                if (currentBackground !== newBackground) {
+                    targetElement.style.background = newBackground;
+                }
+            };
+
+            img.onerror = () => {
+                console.error(`Ошибка загрузки изображения: ${imgBackground}`);
+            };
         }
     }
-}, 1000);
+}, 500);
