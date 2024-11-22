374b76c778c8a7ee753e5019bc790d88adfaf93c
Diramix <79011730+Diramix@users.noreply.github.com>
2024-11-22 06:36:31 +0300

    Change font in to sync lyrics

--- Spotify Music! II/script.js
+++ Spotify Music! II/script.js
@@ -383,3 +383,22 @@ setInterval(() => {
     });
 }, 100);
 /*--------------------------------------------*/
+
+// Google Noto Sans Font
+/*--------------------------------------------*/
+const link1 = document.createElement('link');
+link1.rel = 'preconnect';
+link1.href = 'https://fonts.googleapis.com';
+document.head.appendChild(link1);
+
+const link2 = document.createElement('link');
+link2.rel = 'preconnect';
+link2.href = 'https://fonts.gstatic.com';
+link2.crossOrigin = 'anonymous';
+document.head.appendChild(link2);
+
+const link3 = document.createElement('link');
+link3.href = 'https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap';
+link3.rel = 'stylesheet';
+document.head.appendChild(link3);
+/*--------------------------------------------*/
